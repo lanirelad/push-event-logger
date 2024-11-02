aef03cbd2f1089867210b5d94fba7ba7b346fa08
lanirelad <lanirelad@gmail.com>
2024-11-02 11:37:47 +0200

    debug

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.register_blueprint(appRoutes)
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000) #, debug=True
     
     
 # "url": "https://github.com/lanirelad/Ansible04/commit/c713ed2971d3b3b29d4df638b42e7d4e0d794d1f",
